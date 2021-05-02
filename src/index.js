099c0639e4af95384382170fbb0ed75c5ad2a9f1
mohan nr <44423216+mohanchowdarynr@users.noreply.github.com>
2021-05-02 22:31:52 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+firebase.initializeApp({
+  apiKey: "AIzaSyBFMQfqZz1XSN8KSr3ovMhs7qYpNWpEoVs",
+  authDomain: "evernote-ca571.firebaseapp.com",
+  projectId: "evernote-ca571",
+  storageBucket: "evernote-ca571.appspot.com",
+  messagingSenderId: "660173913224",
+  appId: "1:660173913224:web:a8a24b34b86d6c6f67333d",
+});
 
+ReactDOM.render(<App />, document.getElementById('root'));
 
---
+++ src/sidebaritem/styles.js
@@ -0,0 +1,18 @@
+const styles = theme => ({
+    listItem: {
+      cursor: 'pointer'
+    },
+    textSection: {
+      maxWidth: '85%'
+    },  
+    deleteIcon: {
+      position: 'absolute',
+      right: '5px',
+      top: 'calc(50% - 15px)',
+      '&:hover': {
+        color: 'black'
+      }
+    }
+  });
+  
+  export default styles;
